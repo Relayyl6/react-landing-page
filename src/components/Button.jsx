ad8ec0554e52e8c93cdf3621b93436758271d9ea
Leonard <Oseghaleleonard39@gmail.com>
2025-04-24 16:13:59 -0700

    The <lg breakpoint finally fit the navbar into a vertical spanning that allows for the actual horizontal nav-bar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,23 +2,23 @@ import ButtonSvg from '../../assets/svg/ButtonSvg'
 
 function Button({ className, href, onClick, children, px, white }) {
 
-    const classes = `button relative flex
+    const classes = `${className || ''} hover:text-blue-600 
+    button relative flex
     items-center justify-center h-11 transition-colors 
     hover:text-color-1 ${px || 'px-7'} 
-    ${white ? 'text-n-8' : 'text-n-1'}
-    ${className || ''} `;
+    ${white ? 'text-[#0E0C15]' : 'text-[#FFFFFF]'}`;
 
     const spanClasses = `relative z-10`
 
     const buttonElement = () => (
-            <button className={classes}>
+            <button className={classes} onClick={onClick}>
                 <span className={spanClasses}>{children}</span>
                 {ButtonSvg(white)}
             </button>
         )
 
     const linkElement = () => (
-           <a href={href} className={classes}>
+            <a href={href} className={classes}>
                 <span className={spanClasses}>{children}</span>
                 {ButtonSvg(white)} 
             </a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,37 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import { brainwave } from '../../assets';
 import { navigation } from '../constants';
 import { useEffect } from 'react';
 import Button from './Button';
+import MenuSvg from '../../assets/svg/MenuSvg';
+import { HamburgerMenu } from '../design/Header';
 
 const Header = () => {
 
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(true)
 
-  useEffect(() => console.log(pathname), [pathname]);
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  }
+
+  const handleClick = () => {
+    setOpenNavigation(false);
+  }
+
+  useEffect(() => {
+    console.log(pathname)
+  } , [pathname]);
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 
-    backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 
-    lg:backdrop-blur-sm">
+    <div className={`fixed top-0 left-0 w-full z-50 bg-[#0E0C15] 
+    backdrop-blur-sm border-b border-[#756d8f] lg:bg-[#0E0C15] 
+    lg:backdrop-blur-sm ${openNavigation ? 'bg-[#0E0C15]' : 'bg-[#0E0C15] opacity-90 backdrop-blur-sm'}`}>
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
             <a className="block w-[12rem] xl:mr-8" href="#hero">
                 <img 
@@ -22,34 +40,37 @@ const Header = () => {
                     height={40} 
                     alt="Brainwave"/>
             </a>
-            <nav className="hidden fixed top=[5rem] left-0 right-0 bottom-0 bg-n-8 
-                            lg:static lg:flex lg:mx-auto 
-                            lg:bg-transparent">
+            <nav className={`${openNavigation ? 'flex' : 'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 bg-[#0E0C15]                           
+            lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
                 <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                     {
                       navigation?.map((item, index) => (
                         <a key={item.id || index} 
                            href={item.url}
                            className={`block relative font-code lg:flex text-2xl uppercase text-n-1 
-                                       transition-colors hover:text-blue-600 ${item.onlyMobile ? 'lg:hidden' : ''} 
-                                       px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-bold 
-                                       ${item.url === pathname.hash ? 'z-2 lg:text-indigo-800' : 'lg:text-amber-50'}`}>
+                                       transition-colors hover:text-blue-400 ${item.onlyMobile ? 'lg:hidden' : ''} 
+                                       px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold 
+                                       ${item.url === pathname.hash ? 'z-2 lg:text-indigo-800' : 'lg:text-amber-50'} 
+                                       lg:leading-5 lg:hover:text-n-[#FFFFFF]`}>
                           {item.title}   
                         </a>
                       ))
                     }
                 </div>
+                <HamburgerMenu/>
             </nav>
             <a 
               href="#signup" 
               className="button hidden mr-8 text-n-1/50 
                          transition-colors hover:text-n-1 
-                         lg:block uppercase">
+                         lg:block uppercase hover:text-blue-600">
               New Account
             </a>
-  
             <Button className="hidden lg:flex uppercase"  href="#login">
                 Sign In
+            </Button>
+            <Button className="ml-auto lg:hidden" px="px-3" onClick={toggleNavigation}>
+                <MenuSvg openNavigation={openNavigation}/>
             </Button>
         </div>
     </div>
